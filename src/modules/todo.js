b29b3e04fbbf1805a949ef034e30524f32fe40f3
Mizakson <maxwellizakson@gmail.com>
2024-02-08 10:07:26 -0500

    Create Task class

---
+++ src/modules/todo.js
@@ -0,0 +1,10 @@
+class Task {
+    constructor(title, description, dueDate, priority, status) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.status = status;
+    }
+
+}

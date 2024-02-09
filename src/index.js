982750607bebb6fcdb8268116a451b90232fca38
Mizakson <maxwellizakson@gmail.com>
2024-02-09 12:46:09 -0500

    Create initial pageOnLoad function

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import Project from "./modules/project.js"
 import Task from "./modules/todo.js"
+import pageOnLoad from "./modules/interface.js";
+
+function init() {
+    pageOnLoad();
+}
+
+init();
--- src/modules/interface.js
+++ src/modules/interface.js
@@ -1,2 +1,25 @@
 import Task from "./todo.js";
 import Project from "./project.js";
+
+function pageOnLoad() {
+    const body = document.querySelector('body');
+    const titleContainer = document.createElement('div');
+    titleContainer.classList.add('title');
+
+    const titleText = document.createElement('h1');
+    titleText.innerHTML = 'ToDo List';
+    titleContainer.appendChild(titleText);
+
+    const titleDescription = document.createElement('p');
+    titleDescription.innerHTML = 'A program by Mizakson';
+    titleContainer.appendChild(titleDescription);
+
+    const projectNumber = document.createElement('p');
+    projectNumber.innerHTML = 'Odin Project JavaScript Course Project #4';
+    titleContainer.appendChild(projectNumber);
+    
+    body.appendChild(titleContainer);
+
+};
+
+export default pageOnLoad;

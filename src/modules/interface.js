641b1b0c96fd7df9401f835f923ee8e9c3b6a161
Mizakson <maxwellizakson@gmail.com>
2024-02-13 13:13:55 -0500

    Create pageOnLoad function
    create empty child methods

---
+++ src/modules/interface.js
@@ -0,0 +1,17 @@
+function pageOnLoad() {
+    createTitle();
+    createNav();
+    createContent();
+}
+
+function createTitle() {
+    
+};
+
+function createNav() {
+
+};
+
+function createContent() {
+
+};

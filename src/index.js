d90eb665cc0e98019c9ff07dca45e91a1a9e8d95
Mizakson <maxwellizakson@gmail.com>
2024-02-19 12:19:24 -0500

    Remove console.log statements

--- src/index.js
+++ src/index.js
@@ -6,7 +6,3 @@ const DEFAULT_PROJECT = new Project('DEFAULT','N/A','N/A','N/A','N/A','N/A');
 let TASK1 = new Task('Go food shopping', '', '', 'mid', 'buy apples, milk');
 
 DEFAULT_PROJECT.additem(TASK1);
-
-TASK1.editDescription('added description');
-
-console.log(TASK1);

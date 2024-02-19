6c443d397a2dcb16b2924c3395d3b7565dcd3a11
Mizakson <maxwellizakson@gmail.com>
2024-02-19 11:54:33 -0500

    Instantiate default project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import Item from "./modules/item.js";
 import Project from "./modules/project.js";
 import Task from "./modules/task.js";
+
+const DEFAULT_PROJECT = new Project('DEFAULT','N/A','N/A','N/A','N/A','N/A');
+const DEFAULT_ITEMS = DEFAULT_PROJECT.items;

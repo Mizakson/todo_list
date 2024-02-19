b41ed7c961b9a48e9850ae36af82b3961311b177
Mizakson <maxwellizakson@gmail.com>
2024-02-19 11:11:14 -0500

    Add item class constructor

--- src/modules/item.js
+++ src/modules/item.js
@@ -1,7 +1,11 @@
 // main item class
 
 class Item {
-
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+        this.status = false;
+    }
 };
 
 export default Item;

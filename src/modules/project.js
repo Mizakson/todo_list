99a93bf207cf31a290ecfb5b39f6888b35d0718f
Mizakson <maxwellizakson@gmail.com>
2024-02-19 11:13:52 -0500

    Add project class constructor

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,7 +1,16 @@
+import Item from "./item.js";
+
 // project class and methods
 
-class Project {
-
+class Project extends Item {
+    constructor(name, description, dueDate, priority, notes) {
+        super(name);
+        this.type = 'project';
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.notes = notes
+    }
 };
 
 export default Project;

b70057efe3030befd7c1532b07cd4c84922ca8d8
Mizakson <maxwellizakson@gmail.com>
2024-02-19 12:49:57 -0500

    Finish basic createHeader function, initialize ui

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import Project from "./modules/project.js";
 import Task from "./modules/task.js";
+import pageOnLoad from "./modules/interface.js";
 
 const DEFAULT_PROJECT = new Project('DEFAULT','N/A','N/A','N/A','N/A','N/A');
 
 let TASK1 = new Task('Go food shopping', '', '2024-06-07', 'mid', 'buy apples, milk');
 
 DEFAULT_PROJECT.additem(TASK1);
+
+function init() {
+    pageOnLoad();
+}
+
+init();
--- src/modules/interface.js
+++ src/modules/interface.js
@@ -1,1 +1,44 @@
 // ui creation functions
+
+// cls === class name
+function createBasicEl(tag, cls, idName, text) {
+    const el = document.createElement(`${tag}`);
+
+    if (cls != '') {
+        el.classList.add(`${cls}`);
+    }
+    
+    if (idName != '') {
+        el.id = idName;
+    }
+    
+    if (text != '') {
+        el.innerText = text;
+    }
+    
+    return el;
+};
+
+
+function createHeader() {
+    const body = document.querySelector('body');
+
+    const header = createBasicEl('div','header','','');
+    const title = createBasicEl('div','title-container','','');
+    const projTitle = createBasicEl('h1','title','project-title','To Do List');
+    const projDesc = createBasicEl('h2','','project-description','Odin Project JavaScript Course Project #4');
+    const projCredits = createBasicEl('p','','project-credits', 'A program by Mizakson');
+
+    title.appendChild(projTitle);
+    title.appendChild(projDesc);
+    title.appendChild(projCredits);
+    header.appendChild(title);
+    body.appendChild(header);
+
+}
+
+function pageOnLoad() {
+    createHeader();
+};
+
+export default pageOnLoad;

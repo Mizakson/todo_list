f67e5d9c2844b20e1263164908b43b7c94eadc62
Mizakson <maxwellizakson@gmail.com>
2024-02-20 09:38:54 -0500

    Create render function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import Project from "./modules/project.js";
 import Task from "./modules/task.js";
 import pageOnLoad from "./modules/interface.js";
 
-const DEFAULT_PROJECT = new Project('DEFAULT','N/A','N/A','N/A','N/A','N/A');
+const DEFAULT_PROJECT = new Project('DEFAULT','N/A','N/A','N/A','N/A');
+const DEFAULT_TASK = new Task('DEFAULT','DESC','DATE','PRIO','NOTES');
+const TEST_PROJ = new Project('TEST', 'DESC', 'DATE', 'PRIO', 'NOTES');
+
+
+DEFAULT_PROJECT.additem(DEFAULT_TASK);
+DEFAULT_PROJECT.additem(TEST_PROJ);
+TEST_PROJ.additem(DEFAULT_TASK);
 
 function addFormItem() {
     const defaultText = 'Edit me...';
@@ -52,6 +59,45 @@ function clearForm() {
     $dueDate.value = "";
 }
 
+function render(arr) {
+    const display = document.querySelector('.display');
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i];
+        let itemEl = document.createElement('div');
+        itemEl.classList.add(`card-${i}`);
+
+            itemEl.innerHTML = `
+                <div class='card-header'>
+                    <h2 class='card-title'>${item.name}</h2>
+                    <h2 class='card-type'>${item.type}</h2>
+                    <h2 class='card-date'>${item.dueDate}</h2>
+                    <button class='expand-${i}'>Edit</button>
+                    <button class='delete-${i}'>Delete</button>
+                </div>
+            `;
+            display.appendChild(itemEl);
+
+        if (item.type === 'project') {
+            for (let j = 0; j < item.items.length; j++) {
+                let subItem = item.items[j];
+                let subItemEl = document.createElement('div');
+                subItemEl.classList.add(`sub-card-${j}`);
+                subItemEl.innerHTML = `
+                <div class='sub-card-header'>
+                    <h2 class='sub-card-title'>${subItem.name}</h2>
+                    <h2 class='sub-card-type'>Task</h2>
+                    <h2 class='sub-card-date'>${subItem.dueDate}</h2>
+                    <button class='sub-expand-${j}'>Edit</button>
+                    <button class='sub-delete-${j}'>Delete</button>
+                </div>
+            `;
+            itemEl.appendChild(subItemEl);
+        }
+    }
+        
+    }
+}
+
 function buttonEvents() {
     document.addEventListener('click', function(e) {
         e.preventDefault();
@@ -60,21 +106,16 @@ function buttonEvents() {
             e.preventDefault();
             addFormItem();
             clearForm();
+            render();
         }
     })
-}
-
-function render() {
-    // render all items in DEFAULT_PROJECT.items
-    for (let i = 0; i < DEFAULT_PROJECT.items; i++) {
-        
-    }
 }
 
 
 function init() {
     pageOnLoad();
     buttonEvents();
+    render(DEFAULT_PROJECT.items);
 }
 
 init();

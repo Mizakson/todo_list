3a833fbeba0834d2d8aa970ff650decc7919bd65
Mizakson <maxwellizakson@gmail.com>
2024-02-22 16:39:25 -0500

    Finish render function
    for now, work on display onclick

--- src/index.js
+++ src/index.js
@@ -10,7 +10,12 @@ const DEFAULT_TASK = new Task('test task','description here','8/6/25','low','jus
 const mainArr = MAIN_PROJECT.items;
 MAIN_PROJECT.additem(DEFAULT_PROJECT);
 MAIN_PROJECT.additem(DEFAULT_TASK);
-// console.log(MAIN_PROJECT.items);
+const NESTED_TASK = new Task('nest task','description here','8/6/29','high','just nesting');
+const NESTED_TASK2 = new Task('nest task','description here','8/6/29','high','just nesting');
+DEFAULT_PROJECT.additem(NESTED_TASK);
+DEFAULT_PROJECT.additem(NESTED_TASK2);
+
+
 
 function createEl(tag, cls, idName, text) {
     const el = document.createElement(tag);
@@ -23,6 +28,8 @@ function createEl(tag, cls, idName, text) {
 // main render funciton
 function render() {
     const display = document.querySelector('.display');
+    
+    // displays the first level of mainArr
     mainArr.forEach(function (item, index, arr,) {
         const card = createEl('div','card',`card-${index}`,'');
         const header = createEl('div','header','','');
@@ -32,9 +39,57 @@ function render() {
         const type = createEl('p','type',`type${index}`,`${item['type']}`);
         const dueDate = createEl('p','dueDate',`dueDate${index}`,`${item['dueDate']}`);
 
+        const info = createEl('div','info',`info-${index}`,'');
+        const description = createEl('p', 'description', `description-${index}`,`${item['description']}`);
+        const priority = createEl('p','prio',`prio-${index}`,`${item['priority']}`);
+        const notes = createEl('p','notes',`notes-${index}`,`${item['notes']}`)
+
+        const childContainer = createEl('div','child-container', `child-container-${index}`,'');
+
+        info.appendChild(description);
+        info.appendChild(priority);
+        info.appendChild(notes);
+        info.style.display = 'none';
+
         if (item['type'] === 'project') {
             const addTask = createEl('button','addTask',`addTask-${index}`,'+');
             btns.appendChild(addTask);
+
+            const childItem = arr[index];
+
+            // the values from the child task objects
+            const childTasks = (Object.entries(childItem)[7][1]);
+            console.log(childTasks);
+
+            childTasks.forEach(function(item, index) {
+                const card = createEl('div','child-card',`child-card${index}`,'');
+                const header = createEl('div','child-header',`child-header-${index}`,'');
+                const btns = createEl('div','child-btns',`child-btns-${index}`,'');
+
+                const name = createEl('h4','child-name',`child-name-${index}`,`${item['name']}`);
+                const dueDate = createEl('p','child-dueDate',`child-dueDate-${index}`,`${item['dueDate']}`);
+                const priority = createEl('p','child-prio',`child-prio-${index}`,`${item['priority']}`);
+
+                const edit = createEl('button','child-edit',`child-edit-${index}`,'Edit');
+                const del = createEl('button','child-delete',`child-delete-${index}`,'Delete');
+                const toggle = createEl('button','child-toggle',`child-toggle-${index}`,'Toggle');
+
+                header.appendChild(name);
+                header.appendChild(dueDate);
+                header.appendChild(priority);
+
+                btns.appendChild(edit);
+                btns.appendChild(del);
+                btns.appendChild(toggle);
+
+                card.appendChild(header);
+                card.appendChild(btns);
+
+                childContainer.appendChild(card);
+
+            }); 
+
+        
         }
 
         const edit = createEl('button','edit',`edit-${index}`,'Edit');
@@ -52,13 +107,15 @@ function render() {
         header.appendChild(btns);
 
         card.appendChild(header);
+        card.appendChild(info);
+        card.appendChild(childContainer);
         display.appendChild(card);
 
-
     })
-        
+
     
 }
+
 
 
 function init() {

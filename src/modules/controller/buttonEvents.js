037186244512e5069aa9143237bf0c443a6a495c
Mizakson <maxwellizakson@gmail.com>
2024-03-11 12:56:34 -0400

    Finish add task button event

--- src/modules/controller/buttonEvents.js
+++ src/modules/controller/buttonEvents.js
@@ -4,11 +4,16 @@ export var btnEvents = (function () {
         let projects = Array.from(document.querySelector('.display').childNodes);
 
         projects.forEach(function(item,index) {
+            let projectEls = Array.from(item.children);
             let header = Array.from(item.childNodes[0].childNodes);
             // console.log(header);
             let btns = Array.from(header[1].children);
             
             // 0 - details, 1 - add task, 2 - delete, 3 - toggle 
+
+            btns[1].addEventListener("click", function(e) {
+                projectEls[1].classList.toggle('active');
+            })
 
             console.log(btns);
         })
